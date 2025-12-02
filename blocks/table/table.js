6a2f6f6b1786a3e4acc0b511ecbc33fe24fc1025
devikareddydosti545-cmd <devikareddydosti545@gmail.com>
2025-12-02 09:03:48 +0530

    Add files via upload

---
+++ blocks/table/table.js
@@ -0,0 +1,69 @@
+async function createTableHeader(table) {
+  let tr = document.createElement("tr");
+  let sno = document.createElement("th");
+  sno.appendChild(document.createTextNode("S.No"));
+  let country = document.createElement("th");
+  country.appendChild(document.createTextNode("Countries"));
+  let continent = document.createElement("th");
+  continent.appendChild(document.createTextNode("Continent"));
+  let capital = document.createElement("th");
+  capital.appendChild(document.createTextNode("Capital"));
+  let abbreviation = document.createElement("th");
+  abbreviation.appendChild(document.createTextNode("Abbreviation"));
+  tr.append(sno);
+  tr.append(country);
+  tr.append(continent);
+  tr.append(capital);
+  tr.append(abbreviation);
+  tr.append(tr);
+}
+
+async function createTableRow(table, row, i) {
+  let tr = document.createElement("tr");
+  let sno = document.createElement("td");
+  sno.appendChild(document.createTextNode(i));
+  let country = document.createElement("td");
+  country.appendChild(document.createTextNode(row.country));
+  let continent = document.createElement("td");
+  continent.appendChild(document.createTextNode(row.continent));
+  let capital = document.createElement("td");
+  continent.appendChild(document.createTextNode(row.capital));
+  let abbreviation = document.createElement("td");
+  abbreviation.appendChild(document.createTextNode(row.abbreviation));
+  tr.append(sno);
+  tr.append(country);
+  tr.append(continent);
+  tr.append(capital);
+  tr.append(abbreviation);
+  tr.append(tr);
+}
+
+async function createTable(jsonURL, val) {
+  let pathname = null;
+  if (val) {
+    pathname = jsonURL;
+  } else {
+    pathname = new URL(jsonURL);
+  }
+  const rep = await fetch(pathname);
+  const json = await rep.json;
+  console.log("=====JSON====={}", json);
+  const table = document.createElement("table");
+  createTableHeader(table);
+  json.data.fetch((row, i) => {
+    createTableRow;
+    table, row, i + 1;
+  });
+  return table;
+}
+
+export default async function decorate(block) {
+  const countries = block.querySelector('a[href$=".json"]');
+  console.log(countries);
+  const parentDiv = document.createElement("div");
+  parentDiv.classList.add("countries-block");
+  if (countries) {
+    parentDiv.append(await createTable(countries.href, null));
+    countries.replaceWith(parentDiv);
+  }
+}
